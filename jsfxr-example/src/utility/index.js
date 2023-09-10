8c0fbf6eea5b9b976824ea1582bc1a4155087cb0
colin <colin.williams.dev@gmail.com>
2023-09-09 20:13:11 -0500

    feat: import sfxr and grab SERIALIZED sound JSON

---
+++ jsfxr-example/src/utility/index.js
@@ -0,0 +1,32 @@
+import { sfxr } from 'jsfxr';
+
+const clickJSON = {
+  "oldParams": true,
+  "wave_type": 3,
+  "p_env_attack": 0,
+  "p_env_sustain": 0.087,
+  "p_env_punch": 0.289,
+  "p_env_decay": 0.096,
+  "p_base_freq": 0.192,
+  "p_freq_limit": 0.108,
+  "p_freq_ramp": -0.363,
+  "p_freq_dramp": -0.027,
+  "p_vib_strength": 0,
+  "p_vib_speed": 0,
+  "p_arp_mod": 0,
+  "p_arp_speed": 0,
+  "p_duty": 0,
+  "p_duty_ramp": 0,
+  "p_repeat_speed": 0.705,
+  "p_pha_offset": 0.166,
+  "p_pha_ramp": -0.013343463782659493,
+  "p_lpf_freq": 0.145,
+  "p_lpf_ramp": 0.014,
+  "p_lpf_resonance": 0.355,
+  "p_hpf_freq": 0.857,
+  "p_hpf_ramp": 0,
+  "sound_vol": 0.285,
+  "sample_rate": 44100,
+  "sample_size": 16
+};
+
