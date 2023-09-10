7fcef8b5e1d35d2e7b837d48aa6a8903ba864820
colin <colin.williams.dev@gmail.com>
2023-09-09 19:59:35 -0500

    feat: cloudinary url and webaudio api

--- jsfxr-example/src/App.js
+++ jsfxr-example/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  /* External Cloudinary `click.wav` URL Hosting */
+  const clickCloudinaryUrl = 'https://res.cloudinary.com/dbdyc4klu/video/upload/v1694301930/click_rqnmgv.wav';
+  /* Web Audio API Object */
+  const clickAudio = new Audio(clickCloudinaryUrl);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
+        <h5>Type your message here!</h5>
+        <input onChange={() => clickAudio.play()}></input>
       </header>
     </div>
   );
